612c159968ce0900d43c95cc201d26a769417dfe
Manabu Ori <manabu.ori@gmail.com>
2026-03-15 16:21:32 +0900

    `s/ctags-demo/wasm-demo/`

--- misc/wasm-demo.js
+++ misc/wasm-demo.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 const path = require('path');
 const mime = { '.html':'text/html', '.js':'application/javascript', '.wasm':'application/wasm' };
 http.createServer((req, res) => {
-  const file = path.join(__dirname, req.url === '/' ? 'ctags-demo.html' : req.url);
+  const file = path.join(__dirname, req.url === '/' ? 'wasm-demo.html' : req.url);
   const ext = path.extname(file);
   fs.readFile(file, (err, data) => {
     if (err) { res.writeHead(404); res.end('not found'); return; }
     res.writeHead(200, {'Content-Type': mime[ext] || 'text/plain'});
     res.end(data);
   });
-}).listen(8766, () => console.log('http://localhost:8766/ctags-demo.html'));
+}).listen(8766, () => console.log('http://localhost:8766/wasm-demo.html'));
