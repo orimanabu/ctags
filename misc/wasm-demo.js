210c445232719aa407d9ea0abf3c3684761ddd53
Manabu Ori <manabu.ori@gmail.com>
2026-03-15 16:20:51 +0900

    Use `__dirname`

--- misc/wasm-demo.js
+++ misc/wasm-demo.js
@@ -1,10 +1,9 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const dir = '/Users/ori/devel/src/github.com/orimanabu/ctags/build-wasm';
 const mime = { '.html':'text/html', '.js':'application/javascript', '.wasm':'application/wasm' };
 http.createServer((req, res) => {
-  const file = path.join(dir, req.url === '/' ? 'ctags-demo.html' : req.url);
+  const file = path.join(__dirname, req.url === '/' ? 'ctags-demo.html' : req.url);
   const ext = path.extname(file);
   fs.readFile(file, (err, data) => {
     if (err) { res.writeHead(404); res.end('not found'); return; }
